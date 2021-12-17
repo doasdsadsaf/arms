f84d2237b7ff332451629d8744d684f6faa15703
dongwei <dongwei@qq.com>
2021-12-17 16:42:04 +0800

    搭建gateway

---
+++ wateway-server/src/main/java/com/my/Application.java
@@ -0,0 +1,16 @@
+package com.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @创建人 dw
+ * @创建时间 2021/12/17
+ * @描述
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
