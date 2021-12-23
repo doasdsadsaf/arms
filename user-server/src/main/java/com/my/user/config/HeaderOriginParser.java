6b22ca19320108e78a6ffa26a290416094fccb04
dongwei <dongwei@qq.com>
2021-12-23 15:22:17 +0800

    Sentinel定义授权规则,限制访问user服务器只能通过gateway

---
+++ user-server/src/main/java/com/my/user/config/HeaderOriginParser.java
@@ -0,0 +1,28 @@
+package com.my.user.config;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.RequestOriginParser;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 获取请求头是否符合规则
+ *
+ * @创建人 dw
+ * @创建时间 2021/12/23
+ * @描述
+ */
+@Component
+public class HeaderOriginParser implements RequestOriginParser {
+    @Override
+    public String parseOrigin(HttpServletRequest request) {
+        // 1.获取请求头
+        String origin = request.getHeader("origin");
+        // 2.非空判断
+        if (StringUtils.isEmpty(origin)) {
+            origin = "blank";
+        }
+        return origin;
+    }
+}
